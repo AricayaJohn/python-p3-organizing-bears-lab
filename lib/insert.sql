8f5720ae105ff741ec460466d5ac6d9f95a3d1a5
John <jaricaya@gmail.com>
2024-09-25 05:29:11 -0700

    updated bool

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Black", "Wild", True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "M", "Brown", "Sleepy", True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 7, "F", "Brown", "Hungry", False);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "Brown", "Bored", True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 1, "F", "Black", "Playful", True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 4, "M", "Green", "Angry", True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 3, "F", "Black", "Happy", True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 20, "M", "black", "aggressive", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Black", "Wild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "M", "Brown", "Sleepy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 7, "F", "Brown", "Hungry", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "Brown", "Bored", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 1, "F", "Black", "Playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 4, "M", "Green", "Angry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 3, "F", "Black", "Happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 20, "M", "black", "aggressive", 0);
